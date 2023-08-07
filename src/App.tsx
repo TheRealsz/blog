d0818897194bdfac305b3c78cb288a867d333029
Robson Diego Andrade <robindiegoa@gmail.com>
2023-08-07 17:33:56 -0300

    Adicionando header e criando componente card

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,25 @@
 
 function App() {
 
-  return (
-    <>
 
-    </>
+  // Blog
+  // Blog com search engine
+  // Blog com CRUD
+  // Add Dark Mode
+  // Posteriormente, autenticação e niveis de autoridade
+  return (
+    <div className="h-full w-full">
+      <header className="flex flex-col items-center w-full gap-8 bg-gradient-to-l from-blue-500 to-purple-500 h-auto py-6">
+        <div className="flex flex-row justify-between w-11/12 items-center text-white font-light">
+          <span>TheReal</span>
+          <span>Blog</span>
+        </div>
+        <div>
+          <input type="text" placeholder="Pesquisar no blog"></input>
+        </div>
+      </header>
+      <main className="flex flex-col gap-3 justify-center items-center"></main>
+    </div>
   )
 }
 
