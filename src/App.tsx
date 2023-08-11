522778c6eb61619ce2a771f36eeed35fb42258cf
unknown <Robindiegoa@gmail.com>
2023-08-10 23:22:36 -0300

    :lipstick: feat: Estilizaçao do card mobile

--- src/App.tsx
+++ src/App.tsx
@@ -10,17 +10,17 @@ function App() {
   // Add Dark Mode
   // Posteriormente, autenticação e niveis de autoridade
   return (
-    <div className="h-full w-full flex flex-col gap-5">
+    <div className="h-full w-full flex flex-col gap-10">
       <header className="flex flex-col items-center w-full gap-8 bg-gradient-to-tl from-blue-400 to-violet-800 h-auto py-8">
         <div className="flex flex-row justify-between w-11/12 items-center text-white font-light">
           <span>TheReal</span>
           <span>Blog</span>
         </div>
         <div className='relative w-full justify-center flex flex-row mb-1'>
-          <div className="absolute inset-y-0 left-2 flex items-center pl-3 pointer-events-none">
+          <div className="absolute inset-y-0 left-3.5 flex items-center pl-3.5 pointer-events-none">
           <AiOutlineSearch className="text-xl text-white font-bold" />
           </div>
-          <input type="text" className="outline-none p-2 w-11/12 rounded-lg pl-8 bg-opacity-25 bg-white text-white placeholder:text-white font-light text-sm" placeholder="Pesquisar no blog"></input>
+          <input type="text" className="outline-none p-2 w-11/12 rounded-lg pl-9 bg-opacity-25 bg-white text-white placeholder:text-white font-light text-sm" placeholder="Pesquisar no blog"></input>
         </div>
       </header>
       <main className="flex flex-col gap-3 justify-center items-center">
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,18 +2,18 @@ import { AiOutlineHeart } from "react-icons/ai";
 
 export default function Card() {
     return (
-        <div className="max-h-72 w-11/12 bg-white flex items-center justify-center">
-            <div className="p-2 w-full flex flex-col gap-7">
+        <div className="max-h-56 w-11/12 bg-white flex items-center justify-center">
+            <div className="m-2 w-full flex flex-col gap-6 h-full">
                 <div className="flex flex-row justify-between items-center">
                     <span className="text-xs text-gray-500">02 de jul de 2023</span>
                     <AiOutlineHeart className="text-violet-700" />
                 </div>
-                <div className="flex flex-col gap-1">
-                    <h3>Title</h3>
-                    <p className="text-sm text-gray-500 text-justify">
-                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe consequatur voluptate sunt, expedita 
-                        sed natus error magni, provident assumenda, impedit sapiente architecto quia veniam quaerat molestias 
-                        quas voluptatum? Quae, voluptate!
+                <div className="flex flex-col gap-2">
+                    <h3 className="text-slate-900 leading-5 text-lg text-left">Agora é oficial: o Windows 11 está vindo</h3>
+                    <p className="text-sm leading-6 text-gray-500">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Vestibulum vestibulum auctor est. Nam vitae finibus ante.
+                        Duis lobortis tellus vel diam fringilla, eu ullamcorper ex iaculis.
                     </p>
                 </div>
             </div>
