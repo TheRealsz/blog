489d0709a1552d0d1d8b17327b7d14dc031e91bc
Robson Diego Andrade <robindiegoa@gmail.com>
2023-08-22 22:04:53 -0300

    :recycle: refactor: separando arquivos em pages

---
+++ frontend/src/pages/Login.tsx
@@ -0,0 +1,13 @@
+
+
+// Criacao de logo
+// Token para senhas? (Para passar no reducer e nao deixar o dado tao sensivel assim)
+const Login = () => {
+    return(
+        <div>
+        </div>
+    )
+
+}
+
+export default Login
