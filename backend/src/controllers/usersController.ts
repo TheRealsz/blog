770f2b96abe4c910e2b7e910715a2139773409b2
Robson Diego Andrade <robindiegoa@gmail.com>
2023-10-18 15:16:47 -0300

    :sparkles: feat: Criacao de metodos para edição e exclusao de usuario e mudanca de senha

--- backend/src/controllers/usersController.ts
+++ backend/src/controllers/usersController.ts
@@ -3,7 +3,7 @@ import { Response, Request } from 'express'
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
 
-export const create = async (req: Request,  res: Response) => {
+export const create = async (req: Request, res: Response) => {
     try {
         // Pegando infos que vem da requisicao no front
         const {
@@ -17,7 +17,7 @@ export const create = async (req: Request,  res: Response) => {
         }
 
         const existingUser = await Users.findOne({ email });
-        if (existingUser) { 
+        if (existingUser) {
             return res.status(400).json({ message: 'Email já cadastrado!' });
         }
 
@@ -40,7 +40,7 @@ export const create = async (req: Request,  res: Response) => {
             password: undefined
         }
         // Status 201 quando se cria algo no banco
-        return res.status(201).json({ user ,message: "Cadastro realizado!" })
+        return res.status(201).json({ user, message: "Cadastro realizado!" })
     }
     catch (err) {
         console.log(err)
@@ -58,33 +58,33 @@ export const login = async (req: Request, res: Response) => {
         if (!email || !password) {
             return res.status(400).json({ message: 'Todos os campos são obrigatorios!' });
         }
-        
+
         // encontrando o email 
         const user = await Users.findOne({ email: email })
 
         // se nao existir usuario com email existente ou a senha for diferente da que está no banco
         if (!user || !(await bcrypt.compare(password, user.password))) {
-            return res.status(404).json({ message: "Email ou senha incorreto"})
+            return res.status(404).json({ message: "Email ou senha incorreto" })
         }
-        
+
         // Criando token juntamente do email
         const token = jwt.sign(
             { email },
             process.env.JWT_SECRET as string,
             { expiresIn: '1w' }
-          );
-        
+        );
+
         // Salvando token no usuario
         user.token = token;
         await user.save()
-        
+
         // Retornando as informações do usuario exceto a senha
         const userWithoutPassword = {
-            ...user.toObject(), 
+            ...user.toObject(),
             password: undefined,
         }
 
-        return res.status(200).json({userWithoutPassword, message: "Login realizado com sucesso!"})
+        return res.status(200).json({ userWithoutPassword, message: "Login realizado com sucesso!" })
 
     }
     catch (err) {
@@ -96,8 +96,8 @@ export const login = async (req: Request, res: Response) => {
 export const logout = async (req: Request, res: Response) => {
     try {
         // Recuperando o Id do usuario
-        const { userId } = req.body
-        
+        const userId = req.params.userId
+
         // Verifica se veio algo no body
         if (!userId) {
             return res.status(400).json({ message: 'Usuario não encontrado!' });
@@ -113,7 +113,7 @@ export const logout = async (req: Request, res: Response) => {
 
         // Modificando o token do usuario para undefined
         user.token = ""
-        await user.save()
+        user.save()
 
         return res.status(200).json({ message: "Logout realizado com sucesso!" })
     }
@@ -125,10 +125,103 @@ export const logout = async (req: Request, res: Response) => {
 
 export const edit = async (req: Request, res: Response) => {
     try {
+        // Recuperando o Id do usuario
+        const userId = req.params.userId
+
+        // Verifica se veio algo no body
+        if (!userId) {
+            return res.status(400).json({ message: 'Usuario não encontrado!' });
+        }
+
+        // Buscando usuario pelo ID
+        const user = await Users.findOne({ _id: userId })
+
+        // Verificando se retornou algum usuario
+        if (!user) {
+            return res.status(400).json({ message: 'Usuario não encontrado!' });
+        }
+
+        // Pegando as informações do body
+        const { newFullname } = req.body
+
+        // Verificando se veio algo no body
+        if (!newFullname) {
+            return res.status(400).json({ message: 'Por favor, insira um nome para poder editar!' });
+        }
+
+        // Alterando o nome do usuario
+        user.fullname = newFullname
+        user.save()
+
+        return res.status(200).json({ message: "Nome alterado com sucesso!" })
 
     }
-    catch(err) {
+    catch (err) {
         console.log(err);
-        return res.status(500).json({ message: "Erro ao deslogar!" })
+        return res.status(500).json({ message: "Erro ao editar!" })
+    }
+}
+
+export const changePassword = async (req: Request, res: Response) => {
+    try {
+        const userId = req.params.userId
+
+        if (!userId) {
+            return res.status(400).json({ message: 'Usuario não encontrado!' });
+        }
+
+        const user = await Users.findOne({ _id: userId })
+
+        if (!user) {
+            return res.status(400).json({ message: 'Usuario não encontrado!' });
+        }
+
+        const { newPassword } = req.body
+
+        if (!newPassword) {
+            return res.status(400).json({ message: 'Por favor, insira uma nova senha!' });
+        }
+
+        // Verificando se a senha é adequada
+        if (newPassword.length < 10 || newPassword.length > 50) {
+            return res.status(400).json({ message: 'Por favor, insira uma senha entre 10 a 50 caracteres!' })
+        }
+
+        // Encriptografando a senha
+        const salt = await bcrypt.genSalt(10)
+        const passwordEncrypted = await bcrypt.hash(newPassword, salt)
+
+        user.password = passwordEncrypted
+        user.save()
+
+        return res.status(200).json({ message: "Senha alterada com sucesso!" })
+
+    }
+    catch (err) {
+        console.log(err);
+        return res.status(500).json({ message: "Erro ao alterar senha!" })
+    }
+}
+
+export const exclude = async (req: Request, res: Response) => {
+    try {
+        const userId = req.params.userId
+
+        if (!userId) {
+            return res.status(400).json({ message: 'Usuario não encontrado!' });
+        }
+
+        const deleteUser = await Users.deleteOne({ _id: userId })
+
+        if (!deleteUser) {
+            return res.status(400).json({ message: 'Erro ao excluir usuario!' });
+        }
+
+        return res.status(200).json({ message: "Usuario excluido com sucesso!" })
+
+    }
+    catch (err) {
+        console.log(err);
+        return res.status(500).json({ message: "Erro ao excluir usuario!" })
     }
 }
--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { create, login, logout } from "../controllers/usersController";
+import { changePassword, create, edit, exclude, login, logout } from "../controllers/usersController";
 import { tokenValidation } from "../middlewares/tokenValidation";
 
 // para criar endpoints e atrelar a um controller especifico
@@ -8,7 +8,10 @@ const router = Router()
 
 router.post("/signup", create)
 router.post("/login", login)
-router.post("/logout", tokenValidation, logout)
+router.post("/logout/:userId", tokenValidation, logout)
+router.put("/edit/:userId", tokenValidation, edit)
+router.put("/edit/password/:userId", tokenValidation, changePassword)
+router.delete("/delete/:userId", tokenValidation, exclude)
 
 // Nessa rota de /user, sera feito um post com os parametros vindos do front e usando eles dentro do meu usersController
 export default router
