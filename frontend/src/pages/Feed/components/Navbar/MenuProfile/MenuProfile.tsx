c00c88dbfbfbb200049099409ce2c3de4f8281a3
Robson Diego Andrade <Robindiegoa@gmail.com>
2024-02-12 17:57:28 -0300

    :sparkles: feat: create a modal on edit profile

--- frontend/src/pages/Feed/components/Navbar/MenuProfile/MenuProfile.tsx
+++ frontend/src/pages/Feed/components/Navbar/MenuProfile/MenuProfile.tsx
@@ -7,6 +7,8 @@ import {
     DropdownMenuSeparator,
     DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
+import EditNameModal from "./EditNameModal/EditNameModal";
+import { Dialog, DialogTrigger } from "@radix-ui/react-dialog";
 
 const MenuProfile = () => {
     const fullname = getUserInfo("fullname")
@@ -17,19 +19,22 @@ const MenuProfile = () => {
         .join("");
 
     return (
-        <>
-            <DropdownMenu >
+        <Dialog>
+            <DropdownMenu modal={false}>
                 <DropdownMenuTrigger className="border-none outline-none">
                     <div className="rounded-full border-2 border-dark-40 flex items-center justify-center w-10 h-10 cursor-pointer md:w-12 md:h-12 md:border-2 xl:w-14 xl:h-14">{fullnameFormatted}</div>
                 </DropdownMenuTrigger>
                 <DropdownMenuContent className="bg-dark-20 text-white border-dark-30">
                     <DropdownMenuLabel>{fullname}</DropdownMenuLabel>
                     <DropdownMenuSeparator className="bg-dark-30" />
-                    <DropdownMenuItem>Editar perfil</DropdownMenuItem>
+                    <DialogTrigger asChild>
+                        <DropdownMenuItem>Editar perfil</DropdownMenuItem>
+                    </DialogTrigger>
                     <DropdownMenuItem>Trocar senha</DropdownMenuItem>
                 </DropdownMenuContent>
             </DropdownMenu>
-        </>
+            <EditNameModal fullnameFormatted={fullnameFormatted} />
+        </Dialog>
     );
 }
 
